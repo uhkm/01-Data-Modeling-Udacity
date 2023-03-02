0355293de8ea8f2e5d1c3bebef2d134f7b4df304
Hari Krishna <hari.krishna28496@gmail.com>
2023-03-02 05:07:07 -0500

    edited the insert for time table 4

--- sql_queries.py
+++ sql_queries.py
@@ -103,6 +103,8 @@ SET location = EXCLUDED.location,
 time_table_insert = ("""
 INSERT INTO time (start_time, hour, day, week, month, year, weekday) 
 VALUES (%s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT (start_time) 
+DO NOTHING
 """)
 
 # FIND SONGS
