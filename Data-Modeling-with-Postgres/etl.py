96aab06f2e5ba2b7bca883649acf846385189e8d
Hari Krishna <hari.krishna28496@gmail.com>
2023-03-02 08:59:28 -0500

    Final touch up

--- Data-Modeling-with-Postgres/etl.py
+++ Data-Modeling-with-Postgres/etl.py
@@ -83,7 +83,7 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=postgres password=1234")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
