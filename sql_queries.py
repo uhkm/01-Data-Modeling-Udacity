77accd8ac833eae15c1971ee503cc4cc6e1cbf50
Hari Krishna <hari.krishna28496@gmail.com>
2023-03-01 22:27:34 -0500

    correcting insert on artists table

--- sql_queries.py
+++ sql_queries.py
@@ -79,7 +79,7 @@ INSERT INTO users (user_id, first_name, last_name, gender, level)
 VALUES (%s, %s, %s, %s, %s)
 ON CONFLICT (user_id)
 DO UPDATE
-SET level =  EXCLUDE.level
+SET level =  EXCLUDED.level
 """)
 
 song_table_insert = ("""
