550e2539bccf361337436c64d4ce1c6f23d6541a
Hari Krishna <hari.krishna28496@gmail.com>
2024-05-01 00:35:44 -0400

    Pushing the project 3,4,5 (In progress)

--- Data-Modeling-with-Postgres/main.py
+++
@@ -1,6 +0,0 @@
-from create_tables import main as mainForDataBaseSchema
-from etl import main as mainForDataProcessing
-
-if __name__ == "__main__":
-    mainForDataBaseSchema()
-    mainForDataProcessing()
